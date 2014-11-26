e498c96cc1f0fca347c23977171909d889587d95
Phil Cockfield <phil@cockfield.net>
2014-11-26 11:46:42 -0800

    Update [package.on_use] => [package.onUse]

--- package.js
+++ package.js
@@ -7,7 +7,7 @@ Package.describe({
 
 
 
-Package.on_use(function (api) {
+Package.onUse(function (api) {
   api.use('http', ['client', 'server']);
   api.use(['templating', 'ui', 'spacebars'], 'client');
   api.use(['coffeescript']);
@@ -15,37 +15,37 @@ Package.on_use(function (api) {
   api.export(['Ctrl', 'Ctrls', 'Util']);
 
   // Generated with: github.com/philcockfield/meteor-package-paths
-  api.add_files('client/tmpl/tmpl.html', 'client');
-  api.add_files('client/ns.js', 'client');
-  api.add_files('client/api.coffee', 'client');
-  api.add_files('client/control/control.coffee', 'client');
-  api.add_files('client/control/definition.coffee', 'client');
-  api.add_files('client/control/instance.coffee', 'client');
-  api.add_files('client/tmpl/tmpl.coffee', 'client');
+  api.addFiles('client/tmpl/tmpl.html', 'client');
+  api.addFiles('client/ns.js', 'client');
+  api.addFiles('client/api.coffee', 'client');
+  api.addFiles('client/control/control.coffee', 'client');
+  api.addFiles('client/control/definition.coffee', 'client');
+  api.addFiles('client/control/instance.coffee', 'client');
+  api.addFiles('client/tmpl/tmpl.coffee', 'client');
 
 });
 
 
 
-Package.on_test(function (api) {
+Package.onTest(function (api) {
   api.use(['mike:mocha-package@0.4.7', 'coffeescript']);
   api.use(['templating', 'ui', 'spacebars'], 'client');
   api.use(['respondly:util']);
   api.use('respondly:ctrl');
 
   // Generated with: github.com/philcockfield/meteor-package-paths
-  api.add_files('tests/client/ctrl/sample.html', 'client');
-  api.add_files('tests/client/_init.coffee', 'client');
-  api.add_files('tests/client/ctrl/sample.coffee', 'client');
-  api.add_files('tests/client/ctrl/sample.styl', 'client');
-  api.add_files('tests/client/control.coffee', 'client');
-  api.add_files('tests/client/dom.coffee', 'client');
-  api.add_files('tests/client/instance-callbacks.coffee', 'client');
-  api.add_files('tests/client/instance-children.coffee', 'client');
-  api.add_files('tests/client/instance-deps.coffee', 'client');
-  api.add_files('tests/client/instance-dom.coffee', 'client');
-  api.add_files('tests/client/instance-events.coffee', 'client');
-  api.add_files('tests/client/instance.coffee', 'client');
-  api.add_files('tests/client/render-tmpl.coffee', 'client');
+  api.addFiles('tests/client/ctrl/sample.html', 'client');
+  api.addFiles('tests/client/_init.coffee', 'client');
+  api.addFiles('tests/client/ctrl/sample.coffee', 'client');
+  api.addFiles('tests/client/ctrl/sample.styl', 'client');
+  api.addFiles('tests/client/control.coffee', 'client');
+  api.addFiles('tests/client/dom.coffee', 'client');
+  api.addFiles('tests/client/instance-callbacks.coffee', 'client');
+  api.addFiles('tests/client/instance-children.coffee', 'client');
+  api.addFiles('tests/client/instance-deps.coffee', 'client');
+  api.addFiles('tests/client/instance-dom.coffee', 'client');
+  api.addFiles('tests/client/instance-events.coffee', 'client');
+  api.addFiles('tests/client/instance.coffee', 'client');
+  api.addFiles('tests/client/render-tmpl.coffee', 'client');
 
 });
